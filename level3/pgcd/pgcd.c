956303bbb8fe1ca0a29e67d469dfb1c396e5e99c
Allen Temunovic <allentemunovic@Allens-MacBook-Pro.local>
2018-04-16 15:18:02 -0700

    Helpful for Studying

---
+++ level3/pgcd/pgcd.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int		ft_min(int a, int b)
+{
+	if (a > b)
+		return (b);
+	else
+		return (a);
+}
+
+int		pgcd(int a, int b)
+{
+	int div;
+	int i;
+
+	div = 1;
+	i = 1;
+	while (i <= ft_min(a, b))
+	{
+		if (a % i == 0 && b % i == 0)
+			div = i;
+		i++;
+	}
+	return (div);
+}
+
+int		main(int ac, char **av)
+{
+	if (ac == 3)
+		printf("%i", pgcd(atoi(av[1]), atoi(av[2])));
+	printf("\n");
+	return (0);
+}
